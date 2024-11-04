b74f89c1df233ce91bbd0e66a23a06c51910a1fb
Horam Zarri <horamzarri@gmail.com>
2024-11-04 10:26:46 +0330

    fix model transform of frag position

--- shaders/default_obj_vs.glsl
+++ shaders/default_obj_vs.glsl
@@ -14,7 +14,7 @@ uniform mat4 projection;
 void main()
 {
     TexCoords = aTexCoords;
-    FragPos = aPos;
+    FragPos = vec3(model * vec4(aPos, 1.0));
     Normal = mat3(transpose(inverse(model))) * aNormal;
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
